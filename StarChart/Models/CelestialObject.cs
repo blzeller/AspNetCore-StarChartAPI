d945aceb4e5e13b5965160c3853c22c2aaf96f45
blzeller <36023357+blzeller@users.noreply.github.com>
2019-08-31 01:14:55 -0400

    module 3.1

--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace StarChart.Models
@@ -9,6 +10,7 @@ namespace StarChart.Models
     public class CelestialObject
     {
         public int ID { get; set; }
+        [Required]
         public string Name { get; set; }
         public int? OrbitedObjectId { get; set; }
         [NotMapped]
