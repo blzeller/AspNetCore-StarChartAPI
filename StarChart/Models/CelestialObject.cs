9aa09c1cf95266b70d7c02ca10ffeca5e232f598
blzeller <36023357+blzeller@users.noreply.github.com>
2019-08-31 01:16:19 -0400

    module 3.2

--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -9,7 +9,7 @@ namespace StarChart.Models
 {
     public class CelestialObject
     {
-        public int ID { get; set; }
+        public int Id { get; set; }
         [Required]
         public string Name { get; set; }
         public int? OrbitedObjectId { get; set; }
